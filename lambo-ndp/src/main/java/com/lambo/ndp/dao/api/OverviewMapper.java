41d91502df3986dd3732931ce393c69264f2e58f
Rain <linqingyu@live.com>
2018-04-17 16:39:02 +0800

    新版首页、分类概览

--- lambo-ndp/src/main/java/com/lambo/ndp/dao/api/OverviewMapper.java
+++ lambo-ndp/src/main/java/com/lambo/ndp/dao/api/OverviewMapper.java
@@ -4,5 +4,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface OverviewMapper {
-    List<Map<String,Object>> queryOverview(Map<String, Object> param);
+    Map<String,Object> getCategoryOverview(Map<String, Object> param);
 }
