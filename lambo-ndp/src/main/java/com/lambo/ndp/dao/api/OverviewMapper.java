60607a0e9a34a77189252f9f67d589a1d477e593
sunzhen086 <stomz@163.com>
2018-04-26 20:00:00 +0800

    删除无用代码

--- lambo-ndp/src/main/java/com/lambo/ndp/dao/api/OverviewMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lambo.ndp.dao.api;
-
-import java.util.List;
-import java.util.Map;
-
-public interface OverviewMapper {
-    List<Map<String,Object>> queryOverview(Map<String, Object> param);
-}
