022fa4cfc2d6db06919eecfe8036c786f54e10c4
cuiyuhua <cuiyh01@163.com>
2018-05-29 14:57:44 +0800

    初始化版本

--- lambo-rest/lambo-rest-manage/src/main/java/com/lambo/rest/manage/controller/MockFileController.java
+++ lambo-rest/lambo-rest-manage/src/main/java/com/lambo/rest/manage/controller/MockFileController.java
@@ -1,5 +1,7 @@
 package com.lambo.rest.manage.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.lambo.common.base.BaseController;
 import com.lambo.common.utils.io.FileUtils;
 import com.lambo.common.utils.mapper.JsonMapper;
@@ -11,22 +13,23 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -63,8 +66,6 @@ public class MockFileController extends BaseController {
             String fileName = (String)saveResultMap.get("name");
             String filePath = FileUtils.path(FileUtils.getWebappPath() + OssConstant.UPLOAD_TEMP_PATH)+ fileName;
 
-            logger.info("filePath="+filePath);
-
             File inputFile = new File(filePath);
             Workbook workbook = null;
             try {
@@ -98,7 +99,7 @@ public class MockFileController extends BaseController {
                 //数据行
                 for(int i=rowStart+1;i<=rowEnd;i++){
                     Row dataRow = sheet.getRow(i);
-                    Map dataMap = new HashMap();
+                    Map dataMap = new LinkedHashMap();
                     for(int j=colStart;j<colEnd;j++){
                         Cell cell = dataRow.getCell(j);
                         if(!isEmpty(cell)){
@@ -127,10 +128,12 @@ public class MockFileController extends BaseController {
                                     }
                                 }
                             }
+
                         }
                     }
                     dataList.add(dataMap);
                 }
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -162,6 +165,7 @@ public class MockFileController extends BaseController {
         }
 
         logger.info("上传文件结束。。。");
+
         String returnJson = "";
         JsonMapper jsonMapper = new JsonMapper();
         if(dataList.size()==1){
@@ -173,4 +177,128 @@ public class MockFileController extends BaseController {
         return new OssResult(OssResultConstant.SUCCESS, returnJson);
 
     }
+
+    @ApiOperation(value = "下载文件" ,notes = "下载文件")
+    @RequestMapping(value = "/get/{name:.+}", method = RequestMethod.GET)
+    @ResponseBody
+    public void get(@PathVariable("name") String name,
+                    @RequestParam(required = true, value = "mockData") String mockData,
+                    HttpServletRequest request, HttpServletResponse response) {
+        logger.info("下载文件开始。。。");
+        String filePath = FileUtils.path(FileUtils.getWebappPath()
+                + OssConstant.UPLOAD_TEMP_PATH)
+                + name + ".xlsx";
+
+        logger.info("filePath=" + filePath);
+        logger.info("mockData=" + mockData);
+
+        SXSSFWorkbook workbook = writeExcel(mockData);
+
+        File file = new File(filePath);
+        file.deleteOnExit();
+        FileOutputStream tempout = null;
+        try {
+            tempout = new FileOutputStream(file);
+            workbook.write(tempout);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            tempout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("下载文件结束。。。");
+        FileUtils.downFile(file, request, response);
+
+    }
+
+    private SXSSFWorkbook writeExcel(String mockData) {
+        logger.info("生成EXCEL开始。。。");
+
+        // 创建Excel
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+
+        if(null!=mockData && !"".endsWith(mockData)){
+            try{
+                JSONArray data = JSONArray.parseArray(mockData);
+
+                if(data.size()>0){
+
+                    //字段行
+                    Row row = sheet.createRow(0);
+                    String rowStr = mockData.substring(mockData.indexOf("{")+1,mockData.indexOf("}"));
+                    String[] rowArr = rowStr.split(",");
+                    int colNum = rowArr.length;
+                    String[][] field = new String[colNum][2];
+                    for(int i=0;i<colNum;i++){
+                        String[] arr = rowArr[i].split(":");
+                        field[i][0] = arr[0].replaceAll("\"", "").replaceAll("\'", "");
+                        if(arr[1].indexOf("\"")!=-1 || arr[1].indexOf("\'")!=-1){
+                            field[i][1] = "String";
+                        }else{
+                            field[i][1] = "BigDecimal";
+                        }
+
+                        //字段名称
+                        Cell  cell = row.createCell(i);
+                        cell.setCellValue(field[i][0]);
+                    }
+
+
+                    //数据行
+                    for (int n= 0; n< data.size(); n++) {
+                        Row row_value = sheet.createRow(n + 1);
+                        JSONObject dataRow = data.getJSONObject(n);
+                        for (int i = 0; i < colNum; i++) {
+                            Cell cell_v = row_value.createCell(i);
+
+                            // 在单元格中输入一些内容
+                            if("BigDecimal".equals(field[i][1])){
+                                cell_v.setCellType(CellType.NUMERIC);
+                                cell_v.setCellValue(dataRow.getBigDecimal(field[i][0]).doubleValue());
+                            }else{
+                                cell_v.setCellValue(dataRow.getString(field[i][0]));
+                            }
+
+                        }
+                    }
+                }
+            }catch(Exception e){
+                logger.info("生成EXCEL失败",e);
+            }
+
+        }
+
+        logger.info("生成EXCEL结束。。。");
+        return workbook;
+    }
+
+    //根据对象获取key及数据类型
+    private String[][] getFieldByJson(JSONObject json){
+
+        int colNum = json.size();
+        String[][] field = new String[colNum][2];
+
+        int k = 0;
+        for (Map.Entry<String, Object> entry : json.entrySet()) {
+            field[k][0] = entry.getKey();
+
+            //数据类型
+            String typeName=entry.getValue().getClass().getName();
+            int length= typeName.lastIndexOf(".");
+            field[k][1] =typeName.substring(length+1);
+
+            k++;
+        }
+
+        return field;
+    }
+
 }
