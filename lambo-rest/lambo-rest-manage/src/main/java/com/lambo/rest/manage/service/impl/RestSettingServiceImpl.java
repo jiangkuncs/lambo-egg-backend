6d2e4a6c74bc57f8387c8d5afba30abf83795d60
zhangxiuchao <911278757@qq.com>
2018-05-31 16:04:37 +0800

    服务修改之前先备份原始数据

--- lambo-rest/lambo-rest-manage/src/main/java/com/lambo/rest/manage/service/impl/RestSettingServiceImpl.java
+++ lambo-rest/lambo-rest-manage/src/main/java/com/lambo/rest/manage/service/impl/RestSettingServiceImpl.java
@@ -2,6 +2,8 @@ package com.lambo.rest.manage.service.impl;
 
 import com.lambo.common.annotation.BaseService;
 import com.lambo.common.base.BaseServiceImpl;
+import com.lambo.common.utils.idgen.IdGenerate;
+import com.lambo.rest.manage.dao.api.RestSettingLogMapper;
 import com.lambo.rest.manage.dao.api.RestSettingMapper;
 import com.lambo.rest.manage.dao.api.RestSettingParamsMapper;
 import com.lambo.rest.manage.model.RestSetting;
@@ -13,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.security.sasl.SaslServer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +36,8 @@ public class RestSettingServiceImpl extends BaseServiceImpl<RestSettingMapper, R
 
     @Autowired
     RestSettingParamsMapper restSettingParamsMapper;
-
+    @Autowired
+    RestSettingLogMapper restSettingLogMapper;
     @Override
     @Transactional
     public Integer insert( RestSetting restSetting, List<RestSettingParams> paramsList){
@@ -52,6 +58,44 @@ public class RestSettingServiceImpl extends BaseServiceImpl<RestSettingMapper, R
     @Override
     @Transactional
     public Integer update(RestSetting restSetting,List<RestSettingParams> paramsList){
+        //更新之前先备份一下
+        String restId=restSetting.getRestId();
+        Map restMap= (Map) query(restId);
+        RestSetting restSettingOld= (RestSetting) restMap.get("restSetting");
+
+        String uuid = IdGenerate.uuid();
+        SimpleDateFormat format0 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        String dateTime = format0.format(date);
+
+        Map  restSetLog=new HashMap();
+        restSetLog.put("logId",uuid);
+        restSetLog.put("restId",restSettingOld.getRestId());
+        restSetLog.put("url",restSettingOld.getUrl());
+        restSetLog.put("dataSource",restSettingOld.getDatasource());
+        restSetLog.put("restSql",restSettingOld.getRestSql());
+        restSetLog.put("mockData",restSettingOld.getMockData());
+        restSetLog.put("note",restSettingOld.getNote());
+        restSetLog.put("time",dateTime);
+        restSetLog.put("ip",null);
+        restSetLog.put("userAgent",null);
+        int count1=restSettingLogMapper.insertRestSetLog(restSetLog);
+        //参数
+        List<RestSettingParams> restSettingParamsList= (List<RestSettingParams>) restMap.get("restSettingParamsList");
+        String uuid1 = IdGenerate.uuid();
+        for(RestSettingParams parm : restSettingParamsList){
+            Map  restSetParamsLog=new HashMap();
+            restSetParamsLog.put("logId",uuid1);
+            restSetParamsLog.put("restId",parm.getRestId());
+            restSetParamsLog.put("paramKey",parm.getParamKey());
+            restSetParamsLog.put("paramName",parm.getParamName());
+            restSetParamsLog.put("necessary",parm.getNecessary());
+            restSetParamsLog.put("defaultValue",parm.getDefaultValue());
+            restSetParamsLog.put("time",dateTime);
+            restSetParamsLog.put("ip",null);
+            restSetParamsLog.put("userAgent",null);
+            int count2=restSettingLogMapper.insertRestSetParamsLog(restSetParamsLog);
+        }
 
         int count =restSettingMapper.updateByPrimaryKeyWithBLOBs(restSetting);
 
